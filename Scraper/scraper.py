0138dd75cf6e30d8dda0ac9a9cbe28d75e6ffafd
ofk <farukarakaya@gmail.com>
2018-11-02 11:41:53 +0300

    getTodaysSolutions

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -24,22 +24,39 @@ def getQuestions():
     return questions
 
 '''
-Return String array if cell is black block is None else the text in the cell by default ""
+Return String array if rect is black block is None else the text in the cell by default ""
 '''
-def getCells():
-    cells = []
+def getRects():
+    rects = []
     for i in range(0, 25):
-        cell = driver.find_element_by_id('cell-id-' + str(i))
-        if cell.get_attribute('class') is "Cell-block--1oNaD":
-            cells.append(None)
+        rect = driver.find_element_by_id('cell-id-' + str(i))
+        if rect.get_attribute('class') is "Cell-block--1oNaD":
+            rects.append(None)
         else:
-            cells.append('')
-    return cells
+            rects.append('')
+    return rects
 
-
+'''
+Return String array if rect is black block is None else the letter in the rect
+'''
 def getTodaysSolutions():
-    pass
+    revealbutton = driver.find_element_by_xpath('/html/body/div/div/div/div[4]/div/main/div[2]/div/div/ul/div[1]/li[2]')
+    revealbutton.click()
+    puzzlereveal = driver.find_element_by_xpath(
+        '/html/body/div/div/div[1]/div[4]/div/main/div[2]/div/div/ul/div[1]/li[2]/ul/li[3]')
+    puzzlereveal.click()
+    reveal = driver.find_element_by_xpath('/html/body/div/div/div[2]/div[2]/article/div[2]/button[2]')
+    reveal.click()
 
-init()
-print(getQuestions())
-close()
+    solutions = []
+    for i in range(1, 26):
+        cell = driver.find_element_by_css_selector("#xwd-board > g:nth-child(3) > g:nth-child(" +str(i)+ ")")
+        if len(cell.text) == 0:
+            solutions.append(None)
+        else:
+            solutions.append(cell.text[len(cell.text) -1 ])
+    return solutions
+
+#init()
+#print(getTodaysSolutions())
+#close()
